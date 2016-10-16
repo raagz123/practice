2dfe6f7a5ccddbe3157e4b681750e5dc127430ce
raagz123 <aluri.venkatraghavender12@gmail.com>
2016-10-16 19:00:04 +0530

    Update guvi_hunter_6.c

--- guvi_hunter_6.c
+++ guvi_hunter_6.c
@@ -3,16 +3,16 @@
 int main(void) {
 
 	int t,i,max=0;
-	int a[100],hash[1000]={0};
+	int arr[100],hash[1000]={0};
 	scanf("%d",&t);
 	for(i=0;i<t;i++)
 	{
-		scanf("%d",&a[i]);
-		if(max<a[i])
-			max=a[i];
+		scanf("%d",&arr[i]);
+		if(max<arr[i])
+			max=rra[i];
 	}
 	for(i=0;i<=max;i++)
-		hash[a[i]]++;
+		hash[arr[i]]++;
 	for(i=0;i<=max;i++)
 	{
 			if(hash[i]>1)
